7bce8f29c79289fe7f89df7442311105da6e0902
Dan <41142318+ddtorre02@users.noreply.github.com>
2018-07-13 10:58:56 +0800

    Update max_defer_remove.sh
    Added a grep -w to accurately remove the domain without using a keyword

--- max_defer_remove.sh
+++ max_defer_remove.sh
@@ -4,7 +4,7 @@ if [ -z "$1" ]
 then
   echo "Domain Invalid"
 else
-  find_domain="$(ls -lah  /var/cpanel/email_send_limits/ | grep $1)"
+  find_domain="$(ls -lah  /var/cpanel/email_send_limits/ | grep -w max_deferfail_$1)"
   if [ -z "$find_domain" ]
   then
     echo -e "\e[31m$1\e[0m is not on max_defers list."
